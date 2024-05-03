6e734e6a0a8aad757f49479e1f15229b9039cf8f
Kiku Uchiha <n.u.10.10@mail.ru>
2024-05-04 02:57:17 +0300

    Добавила метод load_notes(загружает заметки из файла JSON, если файл существует)"

--- my.py
+++ my.py
@@ -1,3 +1,7 @@
+import json
+import os
+import datetime
+
 class Note:
     def __init__(self, id, title, body, timestamp):
         self.id = id
@@ -9,3 +13,11 @@ class NoteManager:
     def __init__(self, filename='notes.json'):
         self.filename = filename
         self.notes = self.load_notes()
+
+    def load_notes(self):
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as file:
+                notes_json = json.load(file)
+                return [Note(n['id'], n['title'], n['body'], n['timestamp']) for n in notes_json]
+        else:
+            return [] 
